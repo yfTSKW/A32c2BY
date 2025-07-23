c291062fcbd829edbc682b15b8818377f2a9fc2d
yfTSKW <114396770+yfTSKW@users.noreply.github.com>
2025-07-23 17:16:39 +0200

    调整了 settings.json 配置文件

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  default = "example-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "vm_name" {
+  default = "example-vm"
+}
+
+variable "vm_size" {
+  default = "Standard_B2s"
+}
+
+variable "admin_username" {
+  default = "aarti"
+}
+
+variable "admin_password" {
+  default = "FSDADU12!2d"
+}
